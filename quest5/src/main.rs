6e67dc4af3ac704f1c9d2098ab4b6cb8945e2254
xcdkz <twopsix@gmail.com>
2026-01-14 06:43:04 +0100

    Quest 5 Part 3

--- quest5/src/main.rs
+++ quest5/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+#[derive(Debug)]
 struct Fishbone {
     id: u32,
     nodes: Vec<FishboneNode>,
@@ -10,6 +11,7 @@ struct SimplifiedFishbone {
     nodes: Vec<u32>,
 }
 
+#[derive(Debug)]
 struct FishboneNode {
     value: u32,
     lhs: Option<u32>,
@@ -33,25 +35,28 @@ fn main() {
         Ok(input) => println!("Part 2: {}", part2(&input)),
         Err(e) => eprintln!("Error: {}", e),
     }
-    // match extract_input_from_file("input/test3.txt") {
-    //     Ok(input) => println!("Test 3: {:?}", part3(&input)),
-    //     Err(e) => eprintln!("Error: {}", e),
-    // }
-    // match extract_input_from_file("input/input3.txt") {
-    //     Ok(input) => println!("Part 3: {}", part3(&input)),
-    //     Err(e) => eprintln!("Error: {}", e),
-    // }
+    match extract_input_from_file("input/test3.txt") {
+        Ok(input) => println!("Test 3: {:?}", part3(&input)),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+    match extract_input_from_file("input/input3.txt") {
+        Ok(input) => println!("Part 3: {}", part3(&input)),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
 
 fn part1(input: &[SimplifiedFishbone]) -> String {
-    let mut fishbone = vec![FishboneNode {
-        value: input[0].nodes[0],
-        lhs: None,
-        rhs: None,
-    }];
+    let mut fishbone = Fishbone {
+        id: input[0].id,
+        nodes: vec![FishboneNode {
+            value: input[0].nodes[0],
+            lhs: None,
+            rhs: None,
+        }],
+    };
     for num in input[0].nodes.iter().skip(1) {
         let mut num_placed = false;
-        for node in &mut fishbone {
+        for node in &mut fishbone.nodes {
             if node.lhs.is_none() && *num < node.value {
                 node.lhs = Some(*num);
                 num_placed = true;
@@ -63,15 +68,16 @@ fn part1(input: &[SimplifiedFishbone]) -> String {
             }
         }
         if !num_placed {
-            fishbone.push(FishboneNode {
+            fishbone.nodes.push(FishboneNode {
                 value: *num,
                 lhs: None,
                 rhs: None,
             });
         }
     }
-    fishbone[0].value.to_string()
+    fishbone.nodes[0].value.to_string()
         + &fishbone
+            .nodes
             .iter()
             .skip(1)
             .map(|node| node.value.to_string())
@@ -81,14 +87,17 @@ fn part1(input: &[SimplifiedFishbone]) -> String {
 fn part2(input: &[SimplifiedFishbone]) -> u64 {
     let mut sword_values = Vec::new();
     for sword in input {
-        let mut fishbone = vec![FishboneNode {
-            value: sword.nodes[0],
-            lhs: None,
-            rhs: None,
-        }];
+        let mut fishbone = Fishbone {
+            id: sword.id,
+            nodes: vec![FishboneNode {
+                value: sword.nodes[0],
+                lhs: None,
+                rhs: None,
+            }],
+        };
         for num in sword.nodes.iter().skip(1) {
             let mut num_placed = false;
-            for node in &mut fishbone {
+            for node in &mut fishbone.nodes {
                 if node.lhs.is_none() && *num < node.value {
                     node.lhs = Some(*num);
                     num_placed = true;
@@ -100,7 +109,7 @@ fn part2(input: &[SimplifiedFishbone]) -> u64 {
                 }
             }
             if !num_placed {
-                fishbone.push(FishboneNode {
+                fishbone.nodes.push(FishboneNode {
                     value: *num,
                     lhs: None,
                     rhs: None,
@@ -108,8 +117,9 @@ fn part2(input: &[SimplifiedFishbone]) -> u64 {
             }
         }
         sword_values.push(
-            (fishbone[0].value.to_string()
+            (fishbone.nodes[0].value.to_string()
                 + &fishbone
+                    .nodes
                     .iter()
                     .skip(1)
                     .map(|node| node.value.to_string())
@@ -122,7 +132,102 @@ fn part2(input: &[SimplifiedFishbone]) -> u64 {
 }
 
 fn part3(input: &[SimplifiedFishbone]) -> u64 {
-    todo!()
+    let mut sword_fishbones = Vec::new();
+    for sword in input {
+        let mut fishbone = Fishbone {
+            id: sword.id,
+            nodes: vec![FishboneNode {
+                value: sword.nodes[0],
+                lhs: None,
+                rhs: None,
+            }],
+        };
+        for num in sword.nodes.iter().skip(1) {
+            let mut num_placed = false;
+            for node in &mut fishbone.nodes {
+                if node.lhs.is_none() && *num < node.value {
+                    node.lhs = Some(*num);
+                    num_placed = true;
+                    break;
+                } else if node.rhs.is_none() && *num > node.value {
+                    node.rhs = Some(*num);
+                    num_placed = true;
+                    break;
+                }
+            }
+            if !num_placed {
+                fishbone.nodes.push(FishboneNode {
+                    value: *num,
+                    lhs: None,
+                    rhs: None,
+                });
+            }
+        }
+        sword_fishbones.push(fishbone);
+    }
+    // for fishbone in &sword_fishbones {
+    //     println!("{:?}", fishbone);
+    // }
+    sword_fishbones.sort_by(|a, b| {
+        let a_quality = a
+            .nodes
+            .iter()
+            .map(|node| node.value.to_string())
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap();
+        let b_quality = b
+            .nodes
+            .iter()
+            .map(|node| node.value.to_string())
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap();
+        if a_quality != b_quality {
+            b_quality.cmp(&a_quality)
+        } else {
+            for i in 0..a.nodes.len() {
+                let a_level_lhs_value = if a.nodes[i].lhs.is_none() {
+                    String::from("")
+                } else {
+                    a.nodes[i].lhs.unwrap().to_string()
+                };
+                let a_level_rhs_value = if a.nodes[i].rhs.is_none() {
+                    String::from("")
+                } else {
+                    a.nodes[i].rhs.unwrap().to_string()
+                };
+                let b_level_lhs_value = if b.nodes[i].lhs.is_none() {
+                    String::from("")
+                } else {
+                    b.nodes[i].lhs.unwrap().to_string()
+                };
+                let b_level_rhs_value = if b.nodes[i].rhs.is_none() {
+                    String::from("")
+                } else {
+                    b.nodes[i].rhs.unwrap().to_string()
+                };
+                let a_level_value =
+                    (a_level_lhs_value + &a.nodes[i].value.to_string() + &a_level_rhs_value)
+                        .parse::<u64>()
+                        .unwrap();
+                let b_level_value =
+                    (b_level_lhs_value + &b.nodes[i].value.to_string() + &b_level_rhs_value)
+                        .parse::<u64>()
+                        .unwrap();
+                if a_level_value != b_level_value {
+                    return b_level_value.cmp(&a_level_value);
+                }
+            }
+            b.id.cmp(&a.id)
+        }
+    });
+    sword_fishbones
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, fishbone)| {
+            acc + (((i as u64) + 1) * (fishbone.id as u64))
+        })
 }
 
 fn extract_input_from_file(file_path: &str) -> Result<Vec<SimplifiedFishbone>, String> {
