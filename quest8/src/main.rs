55225839841ccd5984a798c9dfe8e64ab9127e0e
xcdkz <twopsix@gmail.com>
2026-01-16 23:53:18 +0100

    q8 part2

--- quest8/src/main.rs
+++ quest8/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+use itertools::Itertools;
+
 fn main() {
     match extract_input_from_file("input/test1.txt") {
         Ok(input) => println!("Test 1: {}", part1(&input, 8)),
@@ -9,23 +11,55 @@ fn main() {
         Ok(input) => println!("Input 1: {}", part1(&input, 32)),
         Err(err) => eprintln!("Error: {}", err),
     }
+    match extract_input_from_file("input/test2.txt") {
+        Ok(input) => println!("Test 2: {}", part2(&input)),
+        Err(err) => eprintln!("Error: {}", err),
+    }
+    match extract_input_from_file("input/input2.txt") {
+        Ok(input) => println!("Input 2: {}", part2(&input)),
+        Err(err) => eprintln!("Error: {}", err),
+    }
 }
 
-fn extract_input_from_file(file_path: &str) -> Result<Vec<u8>, String> {
+fn extract_input_from_file(file_path: &str) -> Result<Vec<u16>, String> {
     fs::read_to_string(file_path)
         .map_err(|e| e.to_string())?
         .trim()
         .split(',')
         .map(|n| {
-            n.parse::<u8>()
+            n.parse::<u16>()
                 .map_err(|e| format!("Couldn't parse number: {n} - {e}"))
         })
         .collect()
 }
 
-fn part1(input: &[u8], nails: u8) -> u32 {
+fn part1(input: &[u16], nails: u16) -> u32 {
     input
-        .windows(2)
-        .filter(|window| window[0].max(window[1]) - window[0].min(window[1]) == nails / 2)
+        .iter()
+        .tuple_windows()
+        .filter(|(a, b)| *a.max(b) - *a.min(b) == nails / 2)
         .count() as u32
+}
+
+fn part2(input: &[u16]) -> u64 {
+    let mut result = 0;
+    let lines = input
+        .windows(2)
+        .map(|window| window.to_vec())
+        .collect::<Vec<Vec<u16>>>();
+    for i in 0..lines.len() {
+        let smaller_point = lines[i].iter().min().unwrap();
+        let larger_point = lines[i].iter().max().unwrap();
+        result += lines
+            .iter()
+            .skip(i + 1)
+            .filter(|window| {
+                ((window[0] < *larger_point && window[0] > *smaller_point)
+                    && (window[1] > *larger_point || window[1] < *smaller_point))
+                    || ((window[1] < *larger_point && window[1] > *smaller_point)
+                        && (window[0] > *larger_point || window[0] < *smaller_point))
+            })
+            .count() as u64;
+    }
+    result
 }
