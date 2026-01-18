3fbe1d110b55552bbee3748b97b2beb54c68e8ab
xcdkz <twopsix@gmail.com>
2026-01-18 15:08:22 +0100

    Quest 8 part3

--- quest8/src/main.rs
+++ quest8/src/main.rs
@@ -2,6 +2,24 @@ use std::fs;
 
 use itertools::Itertools;
 
+struct Line {
+    a: u16,
+    b: u16,
+}
+
+impl Line {
+    fn new(a: u16, b: u16) -> Self {
+        Line {
+            a: a.min(b),
+            b: a.max(b),
+        }
+    }
+    fn cuts(&self, line: &Line) -> bool {
+        ((self.a < line.b && self.a > line.a) && (self.b > line.b))
+            || ((self.b > line.a && self.b < line.b) && (self.a < line.a))
+    }
+}
+
 fn main() {
     match extract_input_from_file("input/test1.txt") {
         Ok(input) => println!("Test 1: {}", part1(&input, 8)),
@@ -19,6 +37,14 @@ fn main() {
         Ok(input) => println!("Input 2: {}", part2(&input)),
         Err(err) => eprintln!("Error: {}", err),
     }
+    match extract_input_from_file("input/test3.txt") {
+        Ok(input) => println!("Test 3: {}", part3(&input, 8)),
+        Err(err) => eprintln!("Error: {}", err),
+    }
+    match extract_input_from_file("input/input3.txt") {
+        Ok(input) => println!("Input 3: {}", part3(&input, 256)),
+        Err(err) => eprintln!("Error: {}", err),
+    }
 }
 
 fn extract_input_from_file(file_path: &str) -> Result<Vec<u16>, String> {
@@ -33,6 +59,25 @@ fn extract_input_from_file(file_path: &str) -> Result<Vec<u16>, String> {
         .collect()
 }
 
+fn part3(input: &[u16], nails: u16) -> u32 {
+    let lines = input
+        .windows(2)
+        .map(|window| Line::new(window[0], window[1]))
+        .collect::<Vec<Line>>();
+    let mut result = 0;
+    for i in 0..nails {
+        for j in i + 1..=nails {
+            result = result.max(
+                lines
+                    .iter()
+                    .filter(|line| Line { a: i, b: j }.cuts(line) || (line.a == i && line.b == j))
+                    .count() as u32,
+            );
+        }
+    }
+    result
+}
+
 fn part1(input: &[u16], nails: u16) -> u32 {
     input
         .iter()
@@ -45,20 +90,13 @@ fn part2(input: &[u16]) -> u64 {
     let mut result = 0;
     let lines = input
         .windows(2)
-        .map(|window| window.to_vec())
-        .collect::<Vec<Vec<u16>>>();
+        .map(|window| Line::new(window[0], window[1]))
+        .collect::<Vec<Line>>();
     for i in 0..lines.len() {
-        let smaller_point = lines[i].iter().min().unwrap();
-        let larger_point = lines[i].iter().max().unwrap();
         result += lines
             .iter()
             .skip(i + 1)
-            .filter(|window| {
-                ((window[0] < *larger_point && window[0] > *smaller_point)
-                    && (window[1] > *larger_point || window[1] < *smaller_point))
-                    || ((window[1] < *larger_point && window[1] > *smaller_point)
-                        && (window[0] > *larger_point || window[0] < *smaller_point))
-            })
+            .filter(|line| line.cuts(&lines[i]))
             .count() as u64;
     }
     result
